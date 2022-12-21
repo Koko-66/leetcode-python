9821e92af92ecc703c1d2aec96884ab2f32d3ab2
K Cisek-Ndlovu <koko@Ks-MacBook-Pro.local>
2022-12-21 14:32:16 +0000

    Fix 'index out of range error and calculations.

--- roman_to_integer/run.py
+++ roman_to_integer/run.py
@@ -9,25 +9,29 @@ while s_invalid:
         print("The number cannot be 0. Please try again")
     if len(s) > 15:
         print("The number you provided is too long. Please try again")
-    for c in set(s):
+    for c in set(s): # check for illegal characters
         if c not in roman_values.keys():
             print(f"The number contains illegal character {c}, please try again")
             break
     else:
         s_invalid = False
-        i = 0
+        i = -1
         num = 0
-        while i < len(s)-1:
-            if s[i] == "I" and s[i+1] == "V" or s[i] == "I" and s[i+1] == "X":
-                num += roman_values[s[i+1]] - roman_values[s[i]]
-                i+=2
-            elif s[i] == "X" and s[i+1] == "L" or s[i] == "X" and s[i+1] == "C":
-                num += roman_values[s[i+1]] - roman_values[s[i]]
-                i+=2
-            elif s[i] == "C" and s[i+1] == "D" or s[i] == "C" and s[i+1] == "M":
-                num += roman_values[s[i+1]] - roman_values[s[i]]
-                i+=2
+        while abs(i) <= len(s):
+            if abs(i) != len(s): # prevent index out of range error
+                num += roman_values[s[i]]
+                if s[i] == "V" and s[i-1] == "I" or s[i] == "X" and s[i-1] == "I":
+                    num -= roman_values[s[i-1]]
+                    i-=2
+                elif s[i] == "L" and s[i-1] == "X" or s[i] == "C" and s[i-1] == "X":
+                    num -= roman_values[s[i-1]]
+                    i-=2
+                elif s[i] == "D" and s[i-1] == "C" or s[i] == "M" and s[i-1] == "C":
+                    num -= roman_values[s[i-1]]
+                    i-=2
+                else:
+                    i-=1
             else:
                 num += roman_values[s[i]]
-                i+=1
+                i-=1
         print(num)
